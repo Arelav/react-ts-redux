c9ff3c12d19809b012ab3078575284724375e87c
Valery Kharshats <valera.kh@gmail.com>
2017-11-16 14:01:00 -0500

    Initial commit

---
+++ src/setupTests.ts
@@ -0,0 +1,13 @@
+import 'raf/polyfill';
+
+// Temporary hack to suppress error
+// https://github.com/facebookincubator/create-react-app/issues/3199
+window.requestAnimationFrame = function (callback) {
+  setTimeout(callback, 0);
+  return 0;
+};
+
+import { configure } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
---
+++ src/types/index.tsx
@@ -0,0 +1,3 @@
+export declare var global: {
+  requestAnimationFrame: any;
+}
